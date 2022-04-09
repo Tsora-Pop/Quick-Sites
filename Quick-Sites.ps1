dc0e815250755f3ac3aadf25932b695236f55ef6
Thomas <35981510+Tsora-Pop@users.noreply.github.com>
2022-04-08 21:20:29 -0400

    Update Quick-Sites.ps1

--- Quick-Sites.ps1
+++ Quick-Sites.ps1
@@ -1,5 +1,6 @@
 <# Quick-Sites Script
 This will mimic startup tabs in a browser and is best used on as as needed basis.
+Author: Tsora Pop
 #>
 $domain = 'twitter' , 'google'
 # $domain variable array. Replace with your specific domains, subdomains, etc
