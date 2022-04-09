486ad303eefa4a2e163b15f2a9bca619becbb4a4
Thomas <35981510+Tsora-Pop@users.noreply.github.com>
2022-04-08 21:07:23 -0400

    Create Quick-Sites.ps1
    Functional example of Quick-Sites script

---
+++ Quick-Sites.ps1
@@ -0,0 +1,10 @@
+<# Quick-Sites Script
+This will mimic startup tabs in a browser and is best used on as as needed basis.
+#>
+$domain = 'twitter' , 'google'
+# $domain variable array. Replace with your specific domains, subdomains, etc
+ForEach ($i in $domain) {
+    # ForEach iteration to go through every value in $domain array as $i
+    Start-Process "www.$i.com"
+    # Start-Process will open the url formed in your default browser
+}
